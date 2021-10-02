ddec1eb6e1039274b9929a2b1301f23d18cebdee
Soufiane Bannouni <soufiane.bannouni@sowit.fr>
2021-10-02 17:25:05 +0100

    create a simple web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleFunc(w http.ResponseWriter, R *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to myawesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello world...")
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3600", nil)
 }
