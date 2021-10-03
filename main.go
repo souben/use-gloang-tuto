3b58aee2f6dbf35c48fa9f163a2834e3252bcf21
Soufiane Bannouni <soufiane.bannouni@sowit.fr>
2021-10-03 02:31:49 +0100

    add simple pages

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, R *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to myawesome site!</h1>")
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type": "text/html")
+	fmt.Fprint(w, "<h1>Welcome to bad site!</h1>")
+	if r.URL.Path == "/"{
+		fmt.Fprint(w, "<h1>Welcome to my awesome sitel</h1>")
+	}else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "get in touch ...")
+	}else{
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Hello my name is ...</h1>")
+	}
 }
+
+
 
 func main() {
 	http.HandleFunc("/", handleFunc)
 	http.ListenAndServe(":3600", nil)
+	
 }
